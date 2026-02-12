f4076260bd36acc6c889ac6d173ad4a95102ee07
serhatunver <unverserhat2@gmail.com>
2026-02-12 13:38:48 +0300

    feat: replace TabsLine with FeedTabs component in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { TabsLine } from '@/components/TabsLine';
+import { FeedTabs } from '@/components/feed-tabs';
 
 export default async function Home() {
   return (
     <div className="flex flex-1 overflow-hidden">
-      <TabsLine />
+      <FeedTabs />
     </div>
   );
 }
