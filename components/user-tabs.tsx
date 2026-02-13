e663e28c18ba8dad36629816ed9f719dde93a2fc
serhatunver <unverserhat2@gmail.com>
2026-02-13 22:28:01 +0300

    feat: update UserTabs component to accept user prop and display user posts

--- components/user-tabs.tsx
+++ components/user-tabs.tsx
@@ -7,7 +7,9 @@ const tabs = [
   { value: 'media', label: 'Media' },
 ];
 
-export function UserTabs() {
+import { User } from '@/lib/data';
+
+export function UserTabs({ user }: { user: User }) {
   return (
     <Tabs defaultValue="posts" className="w-full">
       <TabsList
@@ -24,11 +26,22 @@ export function UserTabs() {
         value="posts"
         // className="max-h-[calc(100vh-36px)] overflow-y-auto"
       >
-        <div className="">
+        {/* <div className="">
           {Array.from({ length: 5 }).map((_, index) => (
             <PostCard key={index} />
           ))}
-        </div>
+        </div> */}
+        {user.posts.length === 0 ? (
+          <div className="p-4 text-center text-muted-foreground">
+            No posts yet.
+          </div>
+        ) : (
+          <div className="">
+            {user.posts.map((post) => (
+              <PostCard key={post._id} post={post} />
+            ))}
+          </div>
+        )}
       </TabsContent>
       <TabsContent value="replies">Replies</TabsContent>
       <TabsContent value="media">Media</TabsContent>
