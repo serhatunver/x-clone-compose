51de73533036400cd2cb764965b8936281284b4b
serhatunver <unverserhat2@gmail.com>
2026-02-13 22:30:01 +0300

    feat: simplify Home component by removing unnecessary wrapper div

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import { FeedTabs } from '@/components/feed-tabs';
 
 export default async function Home() {
-  return (
-    <div className="flex flex-1 overflow-hidden">
-      <FeedTabs />
-    </div>
-  );
+  return <FeedTabs />;
 }
