ddaef835bdd9da6d22315cc631bc834abb3730c0
serhatunver <unverserhat2@gmail.com>
2026-02-23 00:08:42 +0300

    fix: correct PostCard import path and clean up UserTabs component

--- components/user-tabs.tsx
+++ components/user-tabs.tsx
@@ -1,5 +1,5 @@
 import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
-import { PostCard } from '@/components/post-card';
+import { PostCard } from '@/components/post-card/post-card';
 
 const tabs = [
   { value: 'posts', label: 'Posts' },
@@ -12,25 +12,14 @@ import { User } from '@/lib/data';
 export function UserTabs({ user }: { user: User }) {
   return (
     <Tabs defaultValue="posts" className="w-full">
-      <TabsList
-        variant="line"
-        className="mx-auto w-full sticky top-0 h-13! border-b"
-      >
+      <TabsList variant="line" className="mx-auto w-full h-13! border-b">
         {tabs.map((tab) => (
           <TabsTrigger key={tab.value} value={tab.value}>
             {tab.label}
           </TabsTrigger>
         ))}
       </TabsList>
-      <TabsContent
-        value="posts"
-        // className="max-h-[calc(100vh-36px)] overflow-y-auto"
-      >
-        {/* <div className="">
-          {Array.from({ length: 5 }).map((_, index) => (
-            <PostCard key={index} />
-          ))}
-        </div> */}
+      <TabsContent value="posts">
         {user.posts.length === 0 ? (
           <div className="p-4 text-center text-muted-foreground">
             No posts yet.
