338dfa8a748845cb14acd73f48d713d00944a0a5
serhatunver <unverserhat2@gmail.com>
2026-02-23 00:08:50 +0300

    feat: enhance ShareButton with tooltip for improved user experience

--- components/share-button.tsx
+++ components/share-button.tsx
@@ -7,22 +7,34 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
 import { Link, Share } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 
 export function ShareButton() {
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon" className="rounded-full">
-          <Share />
-        </Button>
-      </DropdownMenuTrigger>
+      <Tooltip>
+        <DropdownMenuTrigger asChild>
+          <TooltipTrigger asChild>
+            <Button variant="ghost" size="icon" className="rounded-full">
+              <Share />
+            </Button>
+          </TooltipTrigger>
+        </DropdownMenuTrigger>
+        <TooltipContent side="bottom">
+          <p>Share</p>
+        </TooltipContent>
+      </Tooltip>
       <DropdownMenuContent
-        // className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
         side={'top'}
-        align="center"
-        sideOffset={0}
+        align="end"
+        sideOffset={4}
+        onCloseAutoFocus={(e) => e.preventDefault()}
       >
         <DropdownMenuItem>
           <Link />
