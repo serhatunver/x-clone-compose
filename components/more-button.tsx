94b933d846b5fafa0c879bf25fea24ade93392ac
serhatunver <unverserhat2@gmail.com>
2026-02-23 00:08:58 +0300

    feat: integrate Tooltip into MoreButton for enhanced user interaction

--- components/more-button.tsx
+++ components/more-button.tsx
@@ -7,24 +7,38 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { Link, Ellipsis, Trash2 } from 'lucide-react';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import { Ellipsis, Trash2 } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 
 export function MoreButton() {
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon" className="rounded-full">
-          <Ellipsis className="text-muted-foreground" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent side={'top'} align="center" sideOffset={0}>
+      <Tooltip>
+        <DropdownMenuTrigger asChild>
+          <TooltipTrigger asChild>
+            <Button variant="ghost" size="icon" className="rounded-full">
+              <Ellipsis className="text-muted-foreground" />
+            </Button>
+          </TooltipTrigger>
+        </DropdownMenuTrigger>
+        <TooltipContent>
+          <p>More</p>
+        </TooltipContent>
+      </Tooltip>
+      <DropdownMenuContent
+        side={'top'}
+        align="end"
+        onCloseAutoFocus={(e) => e.preventDefault()}
+      >
         <DropdownMenuItem className="text-red-600 hover:text-red-600!">
           <Trash2 className="text-red-600" />
           Delete
         </DropdownMenuItem>
-        {/* <DropdownMenuSeparator />
-        <DropdownMenuItem>Log out</DropdownMenuItem> */}
       </DropdownMenuContent>
     </DropdownMenu>
   );
