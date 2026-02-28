ea3894d028066779f789959f047ac25186c07752
serhatunver <unverserhat2@gmail.com>
2026-02-28 22:58:18 +0300

    feat: simplify ShareButton component by removing Tooltip and restructuring DropdownMenu

--- frontend/components/share-button.tsx
+++ frontend/components/share-button.tsx
@@ -1,48 +1,25 @@
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from '@/components/ui/tooltip';
+import { DropdownMenu } from '@/components/dropdown-menu';
 import { Link, Share } from 'lucide-react';
-import { Button } from '@/components/ui/button';
+
+const shareOptions = [
+  {
+    label: 'Copy Link',
+    icon: Link,
+    onSelect: () => {
+      console.log('Copy Link');
+    },
+  },
+  {
+    label: 'Share Post via ...',
+    icon: Share,
+    onSelect: () => {
+      console.log('Share Post via ...');
+    },
+  },
+];
 
 export function ShareButton() {
   return (
-    <DropdownMenu>
-      <Tooltip>
-        <DropdownMenuTrigger asChild>
-          <TooltipTrigger asChild>
-            <Button variant="ghost" size="icon" className="rounded-full">
-              <Share />
-            </Button>
-          </TooltipTrigger>
-        </DropdownMenuTrigger>
-        <TooltipContent side="bottom">
-          <p>Share</p>
-        </TooltipContent>
-      </Tooltip>
-      <DropdownMenuContent
-        side={'top'}
-        align="end"
-        sideOffset={4}
-        onCloseAutoFocus={(e) => e.preventDefault()}
-      >
-        <DropdownMenuItem>
-          <Link />
-          Copy Link
-        </DropdownMenuItem>
-        {/* <DropdownMenuSeparator />
-        <DropdownMenuItem>Log out</DropdownMenuItem> */}
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <DropdownMenu items={shareOptions} triggerIcon={Share} tooltip="Share" />
   );
 }
