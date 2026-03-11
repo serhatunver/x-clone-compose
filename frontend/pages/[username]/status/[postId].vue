30e9d3150de0bc152b259389a669298e1325ad7d
serhatunver <unverserhat2@gmail.com>
2026-03-11 22:43:23 +0300

    Refactor PostDetail, RightSide, SidebarNav, HeaderNav, and status page components for improved structure and styling
    - Updated PostDetail.vue to enhance layout and added conditional rendering for post images.
    - Refactored RightSide.vue to include suggested users dynamically and improved layout.
    - Cleaned up SidebarNav.vue by removing unused items and ensuring proper alignment.
    - Enhanced HeaderNav.vue with consistent styling and structure.
    - Improved status page layout for better responsiveness and organization of components.

--- frontend/pages/[username]/status/[postId].vue
+++ frontend/pages/[username]/status/[postId].vue
@@ -7,36 +7,36 @@ const postsStore = usePostsStore();
 const route = useRoute();
 
 onBeforeMount(async () => {
-  await postsStore.fetchPost(route.params.postId);
+	await postsStore.fetchPost(route.params.postId);
 });
 </script>
 
 <template>
-  <div
-    class="flex lg:w-[920px] min-[1084px]:w-[990px] min-[1384px]:w-[1050px] justify-between"
-  >
-    <div
-      v-if="postsStore.loading"
-      class="flex h-screen justify-center items-center mx-auto"
-    >
-      <Icon name="svg-spinners:90-ring" size="32" class="text-blue-500" />
-    </div>
-    <div v-else>
-      <HeaderNav :content="`Post`" />
-      <PostDetail
-        :post="postsStore.post"
-        class="flex flex-col w-full min-[684px]:w-[600px] border-x-[1px]"
-      />
-      <CreatePost isComment="true" :postId="postsStore.post._id" />
-      <Post
-        v-for="comment in postsStore.post.comments"
-        :post="comment"
-        :key="comment._id"
-      />
-    </div>
-    <!-- {{ post.comments }} -->
-    <RightSide />
-  </div>
+	<div
+		class="flex lg:w-[920px] min-[1084px]:w-[990px] min-[1384px]:w-[1050px] justify-between"
+	>
+		<div
+			v-if="postsStore.loading"
+			class="flex h-screen justify-center items-center mx-auto"
+		>
+			<Icon name="svg-spinners:90-ring" size="32" class="text-blue-500" />
+		</div>
+		<div v-else class="border-x">
+			<HeaderNav :content="`Post`" />
+			<PostDetail
+				:post="postsStore.post"
+				class="flex flex-col w-full min-[684px]:w-[600px]"
+			/>
+			<CreatePost isComment="true" :postId="postsStore.post._id" />
+			<Post
+				v-for="comment in postsStore.post.comments"
+				:post="comment"
+				:key="comment._id"
+			/>
+		</div>
+		<!-- {{ post.comments }} -->
+		<RightSide />
+	</div>
 </template>
 
 <style scoped></style>
