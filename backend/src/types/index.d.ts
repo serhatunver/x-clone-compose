6c18b2e6cae48248dc56f5d07295080ee183be02
serhatunver <unverserhat2@gmail.com>
2026-03-29 02:29:36 +0300

    refactor(types): update Express Request and cleanup
    Adds user ID and cookies to Express Request type for better typing
    support. Removes redundant JWT payload types and renames types file
    for clarity.

--- backend/src/types/index.d.ts
+++ backend/src/types/index.d.ts
@@ -1,18 +1,15 @@
-import { Types } from 'mongoose';
 import { IUser } from '#/modules/user/user.model.js';
 
 declare global {
   namespace Express {
     interface Request {
       user: Pick<IUser, '_id' | 'username'>;
+      id: string;
+      cookies: {
+        'auth.token': string;
+        [key: string]: string;
+      };
     }
-  }
-}
-
-declare module 'jsonwebtoken' {
-  export interface JwtPayload {
-    userId: Types.ObjectId;
-    username: string;
   }
 }
 
